350bcbb0db940a369a29f78abbd399faac71bbe2
GitHub Action <action@github.com>
2024-01-21 02:11:03 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const JSONList = [
     {
-        "name": "One Target Homework HAP2-4-1 - 18 Jan 2024",
+        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
         "password": "",
-        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>conservative | resistant to change<br>appropriate | suitable for a particular person, place, or situation<br>disinterested | unaffected by concern for one's own welfare<br>chaotic | completely unordered and unpredictable and confusing",
-        "id": "vocabulary-p2english-onetargethap241",
+        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
+        "id": "vocabulary-p5chinese-zhonghuapri001",
         "mode": "vocabulary"
     },
     {
@@ -14,13 +14,6 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework HAP2-4-2 - 20 Jan 2024",
-        "password": "",
-        "describe": "inconceivable | totally unlikely<br>stimulate | cause to act in a specified manner<br>essential | basic and fundamental<br>beneficial | promoting or enhancing well-being<br>correlation | a reciprocal connection between two or more things<br>spacious | having ample room<br>relation | the state or quality of having something in common<br>recital | a public instance of repeating something prepared in advance<br>spatial | pertaining to the expanse in which things are located<br>enhance | increase",
-        "id": "vocabulary-p2english-onetargethap242",
-        "mode": "vocabulary"
-    },
-    {
         "name": "五年级高级华文听写表（上册）- 第二课:身体会说话",
         "password": "",
         "describe": "答案 | 答案<br>代表 | 代表<br>昏暗 | 昏暗<br>电脑 | 电脑<br>保持 | 保持<br>户外活动 | 户外活动<br>而且 | 而且<br>放松心情 | 放松心情<br>盯着不放 | 盯着不放<br>眼圈 | 眼圈<br>伤害 | 伤害<br>习惯 | 习惯<br>小明不但学习成绩好 | 小明不但学习成绩好<br>而且还乐于助人 | 而且还乐于助人<br>大家都很喜欢他 | 大家都很喜欢他<br>一到游乐场 | 一到游乐场<br>妹妹的眼睛就亮了起来 | 妹妹的眼睛就亮了起来<br>把回家这件事抛到了九霄云外 | 把回家这件事抛到了九霄云外",
@@ -28,10 +21,24 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
+        "name": "One Target Homework HAP2-4-1 - 18 Jan 2024",
         "password": "",
-        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
-        "id": "vocabulary-p5chinese-zhonghuapri001",
+        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>conservative | resistant to change<br>appropriate | suitable for a particular person, place, or situation<br>disinterested | unaffected by concern for one's own welfare<br>chaotic | completely unordered and unpredictable and confusing",
+        "id": "vocabulary-p2english-onetargethap241",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework HAP2-4-2 - 20 Jan 2024",
+        "password": "",
+        "describe": "inconceivable | totally unlikely<br>stimulate | cause to act in a specified manner<br>essential | basic and fundamental<br>beneficial | promoting or enhancing well-being<br>correlation | a reciprocal connection between two or more things<br>spacious | having ample room<br>relation | the state or quality of having something in common<br>recital | a public instance of repeating something prepared in advance<br>spatial | pertaining to the expanse in which things are located<br>enhance | increase",
+        "id": "vocabulary-p2english-onetargethap242",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework JAM List 3",
+        "password": "",
+        "describe": "epitome | A perfect example of a particular quality or type.<br>dissect | To cut open and examine the structure of a dead animal or plant.",
+        "id": "vocabulary-p2english-otelist3",
         "mode": "vocabulary"
     },
     {
