aa604e762d2a1eaa7c82d7c64a3299412bb19b31
GitHub Action <action@github.com>
2024-01-22 07:23:28 +0000

    Update database.js

--- database.js
+++ database.js
@@ -49,7 +49,7 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework HAP2-4-4 - 23 Jan 2024",
+        "name": "One Target Homework HAP244 - 23 Jan 2024",
         "password": "",
         "describe": "keen | intense or sharp<br>smart | characterized by quickness and ease in learning<br>reluctant | not eager<br>disinterested | unaffected by concern for one's own welfare<br>bitterly | extremely and sharply<br>severe | very harsh or strict, especially when dealing with others<br>furiously | in a manner marked by extreme or violent energy<br>bargain | an agreement between parties fixing obligations of each<br>sensible | able to feel or perceive<br>appropriate | suitable for a particular person, place, or situation",
         "id": "vocabulary-p2english-onetargethap244",
