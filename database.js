cddd8e80cddffd981bb19a470136af49eab2f119
GitHub Action <action@github.com>
2024-04-07 11:10:23 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const JSONList = [
     {
-        "name": "One Target Homework JAM List 20240122",
+        "name": "One Target Homework HAP241 - 18 Jan 2024",
         "password": "",
-        "describe": "parable | A simple story used to illustrate a moral or spiritual lesson.<br>fable | A short story, typically with animals as characters, conveying a moral.<br>ridicule | The act of making fun of someone or something in a cruel or harsh way.<br>image | A representation of the external form of a person or thing in art or a visual likeness captured by a camera.<br>tyrant | A cruel and oppressive ruler.<br>arbiter | A person who settles a dispute or has ultimate authority in a matter.<br>characteristic | A typical or noticeable quality of someone or something.<br>aggressive | Behaving in a forceful or hostile way; ready to attack or confront.<br>prohibited | Formally forbidden by law, rule, or other authority.<br>naive | Lacking experience, wisdom, or judgement; innocently unaware.",
-        "id": "vocabulary-p2english-onetargetjam20240122",
+        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by a law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>conservative | resistant to change<br>appropriate | suitable for a particular person, place, or situation<br>disinterested | unaffected by concern for one's own welfare",
+        "id": "vocabulary-p2english-onetargethap241",
         "mode": "vocabulary"
     },
     {
@@ -14,17 +14,31 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework HAP243 - 22 Jan 2024",
+        "name": "One Target Homework HAP245 - 24 Jan 2024",
         "password": "",
-        "describe": "harmful | causing or capable of causing damage<br>swamp | low land that is seasonally flooded<br>menu | a list of dishes available at a restaurant<br>recipe | directions for making something<br>manual | of or relating to the hands<br>ingredient | a component of a mixture or compound<br>grace | elegance and beauty of movement or expression<br>horror | intense and profound fear<br>delight | a feeling of extreme pleasure or satisfaction<br>peaceful | not disturbed by strife or turmoil or war",
-        "id": "vocabulary-p2english-onetargethap243",
+        "describe": "conservative | resistant to change<br>focus | the concentration of attention or energy on something<br>manual | of or relating to the hands<br>epitome | a standard or typical example<br>renaissance | a period of renewed activity and prominence<br>jester | a clown employed to entertain a king or nobleman<br>dissect | cut open or cut apart<br>prelude | something that introduces what follows<br>grapple | work hard to come to terms with or deal with something<br>embrace | squeeze tightly in your arms, usually with fondness",
+        "id": "vocabulary-p2english-onetargethap245",
         "mode": "vocabulary"
     },
     {
-        "name": "二年级华文听写3 - 第三课",
+        "name": "Zhonghua Homework P5 Spelling List 1 Unit 4 - 11 April 2024",
         "password": "",
-        "describe": "耳 | 耳<br>草 | 草<br>舌头 | 舌头<br>非常 | 非常<br>卡片 | 卡片<br>一杯汽水 | 一杯汽水<br>小羊没有家 | 小羊没有家<br>主人把杯子拿给弟弟 | 主人把杯子拿给弟弟<br>主人 | 主人",
-        "id": "vocabulary-p2chinese-zhonghuapri003",
+        "describe": "sprang out | sprang out<br>burrow | burrow<br>special | special<br>promising | promising<br>demanded | demanded<br>hastily | hastily<br>immediately | immediately<br>intriguing | intriguing<br>signboard | signboard<br>fantasised | fantasised<br>tantalising treats | tantalising treats<br>painstakingly | painstakingly<br>absurd | absurd<br>normally | normally<br>shocked | shocked<br>scrutinised | scrutinised<br>unbelievably | unbelievably<br>puzzled | puzzled<br>narrowed | narrowed<br>innocently | innocently",
+        "id": "vocabulary-p5english-zhonghuapri004",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
+        "password": "",
+        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
+        "id": "vocabulary-p5chinese-zhonghuapri001",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework JAM List 20240122",
+        "password": "",
+        "describe": "parable | A simple story used to illustrate a moral or spiritual lesson.<br>fable | A short story, typically with animals as characters, conveying a moral.<br>ridicule | The act of making fun of someone or something in a cruel or harsh way.<br>image | A representation of the external form of a person or thing in art or a visual likeness captured by a camera.<br>tyrant | A cruel and oppressive ruler.<br>arbiter | A person who settles a dispute or has ultimate authority in a matter.<br>characteristic | A typical or noticeable quality of someone or something.<br>aggressive | Behaving in a forceful or hostile way; ready to attack or confront.<br>prohibited | Formally forbidden by law, rule, or other authority.<br>naive | Lacking experience, wisdom, or judgement; innocently unaware.",
+        "id": "vocabulary-p2english-onetargetjam20240122",
         "mode": "vocabulary"
     },
     {
@@ -42,17 +56,10 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
+        "name": "One Target Homework HAP243 - 22 Jan 2024",
         "password": "",
-        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
-        "id": "vocabulary-p5chinese-zhonghuapri001",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "One Target Homework HAP241 - 18 Jan 2024",
-        "password": "",
-        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by a law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>conservative | resistant to change<br>appropriate | suitable for a particular person, place, or situation<br>disinterested | unaffected by concern for one's own welfare",
-        "id": "vocabulary-p2english-onetargethap241",
+        "describe": "harmful | causing or capable of causing damage<br>swamp | low land that is seasonally flooded<br>menu | a list of dishes available at a restaurant<br>recipe | directions for making something<br>manual | of or relating to the hands<br>ingredient | a component of a mixture or compound<br>grace | elegance and beauty of movement or expression<br>horror | intense and profound fear<br>delight | a feeling of extreme pleasure or satisfaction<br>peaceful | not disturbed by strife or turmoil or war",
+        "id": "vocabulary-p2english-onetargethap243",
         "mode": "vocabulary"
     },
     {
@@ -63,17 +70,17 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework HAP245 - 24 Jan 2024",
-        "password": "",
-        "describe": "conservative | resistant to change<br>focus | the concentration of attention or energy on something<br>manual | of or relating to the hands<br>epitome | a standard or typical example<br>renaissance | a period of renewed activity and prominence<br>jester | a clown employed to entertain a king or nobleman<br>dissect | cut open or cut apart<br>prelude | something that introduces what follows<br>grapple | work hard to come to terms with or deal with something<br>embrace | squeeze tightly in your arms, usually with fondness",
-        "id": "vocabulary-p2english-onetargethap245",
-        "mode": "vocabulary"
-    },
-    {
         "name": "One Target Homework HAP242 - 20 Jan 2024",
         "password": "",
         "describe": "inconceivable | totally unlikely<br>stimulate | cause to act in a specified manner<br>essential | basic and fundamental<br>beneficial | promoting or enhancing well-being<br>correlation | a reciprocal connection between two or more things<br>spacious | having ample room<br>relation | the state or quality of having something in common<br>recital | a public instance of repeating something prepared in advance<br>spatial | pertaining to the expanse in which things are located<br>enhance | increase",
         "id": "vocabulary-p2english-onetargethap242",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "二年级华文听写3 - 第三课",
+        "password": "",
+        "describe": "耳 | 耳<br>草 | 草<br>舌头 | 舌头<br>非常 | 非常<br>卡片 | 卡片<br>一杯汽水 | 一杯汽水<br>小羊没有家 | 小羊没有家<br>主人把杯子拿给弟弟 | 主人把杯子拿给弟弟<br>主人 | 主人",
+        "id": "vocabulary-p2chinese-zhonghuapri003",
         "mode": "vocabulary"
     },
     {
