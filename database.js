ae7dd5cb9b1e54a4b7415c465a2b22c018eab688
GitHub Action <action@github.com>
2025-05-06 07:29:53 +0000

    Update database.js

--- database.js
+++ database.js
@@ -1,51 +1,9 @@
 const JSONList = [
     {
-        "name": "Zhonghua Homework P5 Spelling List 3 - 1 Aug 2024",
+        "name": "One Target Homework JAM List 20240120",
         "password": "",
-        "describe": "alleviate | alleviate<br>presence | presence<br>lift their spirits | lift their spirits<br>keen sense of smell | keen sense of smell<br>decomposing | decomposing<br>potential | potential<br>human counterparts | human counterparts<br>intensive training | intensive training<br>handlers | handlers<br>investigation | investigation<br>therapy | therapy<br>inquisitive | inquisitive<br>tenacious | tenacious<br>tough circumstances | tough circumstances<br>endure hardship | endure hardship<br>responsive | responsive<br>immediate situations | immediate situations<br>amidst distractions | amidst distractions<br>competent | competent<br>invaluable | invaluable",
-        "id": "vocabulary-p5english-zhonghuapri005",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
-        "password": "",
-        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
-        "id": "vocabulary-p5chinese-zhonghuapri001",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "One Target Homework JAM List 20240122",
-        "password": "",
-        "describe": "parable | A simple story used to illustrate a moral or spiritual lesson.<br>fable | A short story, typically with animals as characters, conveying a moral.<br>ridicule | The act of making fun of someone or something in a cruel or harsh way.<br>image | A representation of the external form of a person or thing in art or a visual likeness captured by a camera.<br>tyrant | A cruel and oppressive ruler.<br>arbiter | A person who settles a dispute or has ultimate authority in a matter.<br>characteristic | A typical or noticeable quality of someone or something.<br>aggressive | Behaving in a forceful or hostile way; ready to attack or confront.<br>prohibited | Formally forbidden by law, rule, or other authority.<br>naive | Lacking experience, wisdom, or judgement; innocently unaware.",
-        "id": "vocabulary-p2english-onetargetjam20240122",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "One Target Homework HAP241 - 18 Jan 2024",
-        "password": "",
-        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by a law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>appropriate | suitable for a particular person, place, or situation",
-        "id": "vocabulary-p2english-onetargethap241",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "Zhonghua Homework P5 Spelling List 1 Unit 4 - 11 April 2024",
-        "password": "",
-        "describe": "sprang out | sprang out<br>burrow | burrow<br>special | special<br>promising | promising<br>demanded | demanded<br>hastily | hastily<br>immediately | immediately<br>intriguing | intriguing<br>signboard | signboard<br>fantasised | fantasised<br>tantalising treats | tantalising treats<br>painstakingly | painstakingly<br>absurd | absurd<br>normally | normally<br>shocked | shocked<br>scrutinised | scrutinised<br>unbelievably | unbelievably<br>puzzled | puzzled<br>narrowed | narrowed<br>innocently | innocently",
-        "id": "vocabulary-p5english-zhonghuapri004",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "One Target Homework HAP244 - 23 Jan 2024",
-        "password": "",
-        "describe": "keen | intense or sharp<br>smart | characterized by quickness and ease in learning<br>reluctant | not eager<br>disinterested | unaffected by concern for one's own welfare<br>bitterly | extremely and sharply<br>severe | very harsh or strict, especially when dealing with others<br>furiously | in a manner marked by extreme or violent energy<br>bargain | an agreement between parties fixing obligations of each<br>appropriate | suitable for a particular person, place, or situation",
-        "id": "vocabulary-p2english-onetargethap244",
-        "mode": "vocabulary"
-    },
-    {
-        "name": "One Target Homework HAP245 - 24 Jan 2024",
-        "password": "",
-        "describe": "conservative | resistant to change<br>focus | the concentration of attention or energy on something<br>manual | of or relating to the hands<br>epitome | a standard or typical example<br>renaissance | a period of renewed activity and prominence<br>jester | a clown employed to entertain a king or nobleman<br>dissect | cut open or cut apart<br>prelude | something that introduces what follows<br>grapple | work hard to come to terms with or deal with something<br>embrace | squeeze tightly in your arms, usually with fondness",
-        "id": "vocabulary-p2english-onetargethap245",
+        "describe": "epitome | A perfect example of a particular quality or type.<br>dissect | To cut open and examine the structure of a dead animal or plant.<br>priceless | Extremely valuable and impossible to measure in terms of money.<br>prolific | Producing a large amount of something; highly productive.<br>flustered | Nervous or confused, especially because of being rushed or overwhelmed.<br>prelude | An introduction or opening to something more important.<br>grapple | To struggle or fight with something or someone, often in a physical or figurative sense.<br>embrace | To hold someone closely in one's arms, typically to express affection; to accept or support willingly and enthusiastically.|<br>reluctantly | With hesitation, doubt or dread; unwillingly.",
+        "id": "vocabulary-p2english-onetargetjam20240120",
         "mode": "vocabulary"
     },
     {
@@ -63,10 +21,10 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework HAP243 - 22 Jan 2024",
+        "name": "Zhonghua Homework P5 Spelling List 3 - 1 Aug 2024",
         "password": "",
-        "describe": "harmful | causing or capable of causing damage<br>swamp | low land that is seasonally flooded<br>menu | a list of dishes available at a restaurant<br>recipe | directions for making something<br>manual | of or relating to the hands<br>ingredient | a component of a mixture or compound<br>grace | elegance and beauty of movement or expression<br>horror | intense and profound fear<br>delight | a feeling of extreme pleasure and satisfaction",
-        "id": "vocabulary-p2english-onetargethap243",
+        "describe": "alleviate | alleviate<br>presence | presence<br>lift their spirits | lift their spirits<br>keen sense of smell | keen sense of smell<br>decomposing | decomposing<br>potential | potential<br>human counterparts | human counterparts<br>intensive training | intensive training<br>handlers | handlers<br>investigation | investigation<br>therapy | therapy<br>inquisitive | inquisitive<br>tenacious | tenacious<br>tough circumstances | tough circumstances<br>endure hardship | endure hardship<br>responsive | responsive<br>immediate situations | immediate situations<br>amidst distractions | amidst distractions<br>competent | competent<br>invaluable | invaluable",
+        "id": "vocabulary-p5english-zhonghuapri005",
         "mode": "vocabulary"
     },
     {
@@ -77,10 +35,17 @@ const JSONList = [
         "mode": "vocabulary"
     },
     {
-        "name": "One Target Homework JAM List 20240120",
+        "name": "One Target Homework JAM List 20240122",
         "password": "",
-        "describe": "epitome | A perfect example of a particular quality or type.<br>dissect | To cut open and examine the structure of a dead animal or plant.<br>priceless | Extremely valuable and impossible to measure in terms of money.<br>prolific | Producing a large amount of something; highly productive.<br>flustered | Nervous or confused, especially because of being rushed or overwhelmed.<br>prelude | An introduction or opening to something more important.<br>grapple | To struggle or fight with something or someone, often in a physical or figurative sense.<br>embrace | To hold someone closely in one's arms, typically to express affection; to accept or support willingly and enthusiastically.|<br>reluctantly | With hesitation, doubt or dread; unwillingly.",
-        "id": "vocabulary-p2english-onetargetjam20240120",
+        "describe": "parable | A simple story used to illustrate a moral or spiritual lesson.<br>fable | A short story, typically with animals as characters, conveying a moral.<br>ridicule | The act of making fun of someone or something in a cruel or harsh way.<br>image | A representation of the external form of a person or thing in art or a visual likeness captured by a camera.<br>tyrant | A cruel and oppressive ruler.<br>arbiter | A person who settles a dispute or has ultimate authority in a matter.<br>characteristic | A typical or noticeable quality of someone or something.<br>aggressive | Behaving in a forceful or hostile way; ready to attack or confront.<br>prohibited | Formally forbidden by law, rule, or other authority.<br>naive | Lacking experience, wisdom, or judgement; innocently unaware.",
+        "id": "vocabulary-p2english-onetargetjam20240122",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "Zhonghua Homework P5 Spelling List 1 Unit 4 - 11 April 2024",
+        "password": "",
+        "describe": "sprang out | sprang out<br>burrow | burrow<br>special | special<br>promising | promising<br>demanded | demanded<br>hastily | hastily<br>immediately | immediately<br>intriguing | intriguing<br>signboard | signboard<br>fantasised | fantasised<br>tantalising treats | tantalising treats<br>painstakingly | painstakingly<br>absurd | absurd<br>normally | normally<br>shocked | shocked<br>scrutinised | scrutinised<br>unbelievably | unbelievably<br>puzzled | puzzled<br>narrowed | narrowed<br>innocently | innocently",
+        "id": "vocabulary-p5english-zhonghuapri004",
         "mode": "vocabulary"
     },
     {
@@ -88,6 +53,41 @@ const JSONList = [
         "password": "",
         "describe": "答案 | 答案<br>代表 | 代表<br>昏暗 | 昏暗<br>电脑 | 电脑<br>保持 | 保持<br>户外活动 | 户外活动<br>而且 | 而且<br>放松心情 | 放松心情<br>盯着不放 | 盯着不放<br>眼圈 | 眼圈<br>伤害 | 伤害<br>习惯 | 习惯<br>小明不但学习成绩好 | 小明不但学习成绩好<br>而且还乐于助人 | 而且还乐于助人<br>大家都很喜欢他 | 大家都很喜欢他<br>一到游乐场 | 一到游乐场<br>妹妹的眼睛就亮了起来 | 妹妹的眼睛就亮了起来<br>把回家这件事抛到了九霄云外 | 把回家这件事抛到了九霄云外",
         "id": "vocabulary-p5chinese-zhonghuapri002",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework HAP245 - 24 Jan 2024",
+        "password": "",
+        "describe": "conservative | resistant to change<br>focus | the concentration of attention or energy on something<br>manual | of or relating to the hands<br>epitome | a standard or typical example<br>renaissance | a period of renewed activity and prominence<br>jester | a clown employed to entertain a king or nobleman<br>dissect | cut open or cut apart<br>prelude | something that introduces what follows<br>grapple | work hard to come to terms with or deal with something<br>embrace | squeeze tightly in your arms, usually with fondness",
+        "id": "vocabulary-p2english-onetargethap245",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework HAP244 - 23 Jan 2024",
+        "password": "",
+        "describe": "keen | intense or sharp<br>smart | characterized by quickness and ease in learning<br>reluctant | not eager<br>disinterested | unaffected by concern for one's own welfare<br>bitterly | extremely and sharply<br>severe | very harsh or strict, especially when dealing with others<br>furiously | in a manner marked by extreme or violent energy<br>bargain | an agreement between parties fixing obligations of each<br>appropriate | suitable for a particular person, place, or situation",
+        "id": "vocabulary-p2english-onetargethap244",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "五年级高级华文听写表（上册）- 第一课:到户外去",
+        "password": "",
+        "describe": "露营 | 露营<br>集合 | 集合<br>鞋袜 | 鞋袜<br>防止 | 防止<br>单独行动 | 单独行动<br>贵重物品 | 贵重物品<br>绑紧 | 绑紧<br>冠军 | 冠军<br>摔倒 | 摔倒<br>相互配合 | 相互配合<br>轮流 | 轮流<br>擦干净 | 擦干净<br>一顿饭 | 一顿饭<br>营火会结束了 | 营火会结束了<br>我虽然很累 | 我虽然很累<br>但是却感到非常充实 | 但是却感到非常充实<br>我打开饭盒 | 我打开饭盒<br>不管三七二十一 | 不管三七二十一<br>狼吞虎咽地吃了起来 | 狼吞虎咽地吃了起来<br>不一会儿 | 不一会儿<br>就把饭菜吃了个精光 | 就把饭菜吃了个精光",
+        "id": "vocabulary-p5chinese-zhonghuapri001",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework HAP241 - 18 Jan 2024",
+        "password": "",
+        "describe": "pollute | contaminate; make impure<br>censor | a person authorized to suppress unacceptable material<br>prohibited | forbidden by law<br>endangered | in imminent threat of extinction<br>stimulate | cause to act in a specified manner<br>recital | a public instance of repeating something prepared in advance<br>appropriate | suitable for a particular person, place, or situation",
+        "id": "vocabulary-p2english-onetargethap241",
+        "mode": "vocabulary"
+    },
+    {
+        "name": "One Target Homework HAP243 - 22 Jan 2024",
+        "password": "",
+        "describe": "harmful | causing or capable of causing damage<br>swamp | low land that is seasonally flooded<br>menu | a list of dishes available at a restaurant<br>recipe | directions for making something<br>manual | of or relating to the hands<br>ingredient | a component of a mixture or compound<br>grace | elegance and beauty of movement or expression<br>horror | intense and profound fear<br>delight | a feeling of extreme pleasure and satisfaction",
+        "id": "vocabulary-p2english-onetargethap243",
         "mode": "vocabulary"
     },
     {
